845aa367cb5d5d3802b41dfa17d94db1ebfaa17c
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-09-07 14:22:13 -0400

    Fixed: Review section rendering more than 50 child elements

--- src/components/ReviewSection/ReviewSection.jsx
+++ src/components/ReviewSection/ReviewSection.jsx
@@ -21,25 +21,27 @@ export default function ReviewSection() {
   const isLoading = mangaReviews.isLoading && animeReviewIsSelected.isLoading;
 
   const reviewType = animeReviewIsSelected ? animeList : mangaList;
-  const reviewCards = reviewType?.data.map((el, idx) => {
-    return (
-      <SwiperSlide className="review-card" key={el.mal_id}>
-        <div className="review-card-header">
-          <div className="user-profile d-flex a-center ">
-            <img src={el.user.images.webp.image_url} alt="23" />
-            <a rel="noreferrer" target="_blank" href={el.user.url}>
-              {el.user.username}
-            </a>
+  const reviewCards = reviewType?.data
+    .filter((el, idx) => idx < 11)
+    .map((el, idx) => {
+      return (
+        <SwiperSlide className="review-card" key={el.mal_id}>
+          <div className="review-card-header">
+            <div className="user-profile d-flex a-center ">
+              <img src={el.user.images.webp.image_url} alt="23" />
+              <a rel="noreferrer" target="_blank" href={el.user.url}>
+                {el.user.username}
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="review-text">{el.review.slice(0, 140) + "..."}</div>
-        <div className="anime-title">
-          <FaEvernote size={13} />
-          {el.entry.title}
-        </div>
-      </SwiperSlide>
-    );
-  });
+          <div className="review-text">{el.review.slice(0, 140) + "..."}</div>
+          <div className="anime-title">
+            <FaEvernote size={13} />
+            {el.entry.title}
+          </div>
+        </SwiperSlide>
+      );
+    });
 
   return (
     <div className="review-section-wrapper d-flex a-center j-center">
