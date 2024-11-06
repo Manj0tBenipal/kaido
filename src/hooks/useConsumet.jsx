ed28ab3a549a166cbc5ea9ffb04fb84730365d4a
Manjot Singh <manjotbenipal00001@gmail.com>
2024-11-06 18:00:54 -0500

    Update useConsumet.jsx

--- src/hooks/useConsumet.jsx
+++ src/hooks/useConsumet.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { servers } from "../api/gogoanime_servers";
 function handleConsumetResponse(endpoint, parameter) {
-  const BASE_URL = "https://api.consumet.manjotbenipal.xyz/anime/gogoanime";
+  const BASE_URL = `https://consumet.manjotbenpipal.xyz/anime/gogoanime`;
   const results = useQuery(`${endpoint}${parameter}`, async () => {
     if (parameter) {
       return await axios
